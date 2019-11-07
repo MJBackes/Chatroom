d2283f43e1d938d7293dc9208c35824767bac82e
MJBackes <mjohnbackes@gmail.com>
2019-11-07 09:46:05 -0600

    Added starter code to repo along with IServer and IClient interfaces.

---
+++ Server/IServer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public interface IServer
+    {
+        void Join(IClient client);
+        void Leave(IClient client);
+        void Notify();
+
+    }
+}
